1ceb3507219ba2466ccf607fb1dd2eb1bc02203c
neolectron <neolectron@codinglab.io>
2021-01-10 21:31:55 +0100

    style(eslint): React must no longer be in scope

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "react/prop-types": "off"
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope" : "off"
     }
 };
