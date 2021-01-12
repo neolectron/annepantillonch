7f5d3061037af476f652443de441eb84be5cea88
neolectron <neolectron@codinglab.io>
2021-01-12 01:48:28 +0100

    fix(page:index): fix animation according to suggestions

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { animated, config,  useTrail } from 'react-spring';
 
 export default function Home() {
-  const [isAnimated, set] = useState(false);
+  const [isAnimated, setAnimated] = useState(false);
   
   useEffect(() => {
-    setTimeout(() => {set(true)}, 1000);
-  },[]);
+    setTimeout(() => setAnimated(true), 1000);
+    const interval = setInterval(() => {
+      setAnimated(x => !x);
+    }, 21 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const [[first, second]] = useTrail(2, () => ({
     from: { opacity: 0, transform: -30 },
@@ -21,20 +26,24 @@ export default function Home() {
   return (
     <Layout menuBgTransparent>
       <div className="relative h-full flex overflow-hidden">
-        <div style={{ backgroundImage: 'url("/technique.png")', transitionDuration: '30s'}}
-          className={`absolute w-full h-full pointer-events-none 
-          bg-cover bg-center transform transition-transform ease-linear ${isAnimated && 'scale-125'}`}>
+
+        <div style={{ backgroundImage: 'url("/technique.png")', transitionDuration: '20s' }}
+          className={`absolute w-full h-full bg-cover pointer-events-none
+          transition-transform transform origin-right ease-linear
+          ${isAnimated ? 'scale-125' : 'scale-100'}`}>
         </div>
+
         <div className="ml-8 mr-4 flex flex-col justify-center text-white">
           <animated.h1 style={{ opacity: first.opacity, transform: first.transform.interpolate(translate) }}
-          className="opacity-0 text-5xl md:text-8xl font-bold px-4 border-b border-opacity-30 border-white">
+            className="opacity-0 text-5xl md:text-8xl font-bold px-4 border-b border-opacity-30 border-white">
             Anne Pantillon
           </animated.h1>
           <animated.h2 style={{ opacity: second.opacity, transform: second.transform.interpolate(translate) }}
-          className="opacity-0 text-2xl text-gray-300 shadow-xl px-4">
-            Artiste Peintre
+            className="opacity-0 text-2xl text-gray-300 shadow-xl px-4">
+            Artiste Plasticienne 
           </animated.h2>
         </div>
+
       </div>
     </Layout>
   )
