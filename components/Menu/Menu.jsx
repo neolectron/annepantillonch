c974486f9b1561148f66415f87602a4d0161ca44
neolectron <neolectron@codinglab.io>
2021-02-03 11:51:32 +0100

    fix(menu): icons

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -47,9 +47,8 @@ const Menu = ({children, Bgtransparent}) => {
       onClick={() => setOpen(!open)}
       ref={menuRef}
     >
-      <div className="w-full h-14 flex items-center justify-between p-2">
-        <img  width="34px" src="/ap.png" style={{ filter: `${Bgtransparent ? 'invert(1)' : ''}` }} alt="logo menu" />
-        <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer" viewBox="0 0 32 32" width="32" height="32" focusable="false">
+      <div className="w-full h-14 flex items-center p-2">
+        <svg xmlns="http://www.w3.org/2000/svg" className="cursor-pointer transform hover:scale-110" viewBox="0 0 32 32" width="32" height="32" focusable="false">
           <path stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeMiterlimit="10" d="M4 7h22M4 15h22M4 23h22"></path>
         </svg>
       </div>
@@ -60,8 +59,7 @@ const Menu = ({children, Bgtransparent}) => {
             {children}
           </nav>
           <div className={`flex ${open ? '' : 'md:flex-col md:ml-0'} ml-14 border-t border-opacity-20 items-start justify-start`}>
-            <Link href="/#">
-              <a className="m-2 transform hover:scale-110">
+            <a href="https://www.linkedin.com/in/anne-pantillon-3b1b7468/" target="_blank" className="m-2 transform hover:scale-110">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                   <path fill="currentColor" d={`M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761
                   2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11
@@ -71,9 +69,7 @@ const Menu = ({children, Bgtransparent}) => {
                   0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z`} />
                 </svg>
               </a>
-            </Link>
-            <Link href="/#">
-              <a className="m-2 transform hover:scale-110">
+            <a href="https://www.instagram.com/anne_pantillon/" target="_blank" className="m-2 transform hover:scale-110">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
                   <path fill="currentColor" d={`M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919
                   4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149
@@ -91,7 +87,13 @@ const Menu = ({children, Bgtransparent}) => {
                   1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z`} />
                 </svg>
               </a>
-            </Link>
+              <a href="https://www.facebook.com/atelier.anne.pantillon" target="_blank" className="m-2 transform hover:scale-110">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 155.139 155.139">
+                  <path fill="currentColor" d={`M89.584,155.139V84.378h23.742l3.562-27.585H89.584V39.184
+                  c0-7.984,2.208-13.425,13.67-13.425l14.595-0.006V1.08C115.325,0.752,106.661,0,96.577,0C75.52,0,61.104,12.853,61.104,36.452
+                  v20.341H37.29v27.585h23.814v70.761H89.584z`} />
+                </svg>
+              </a>
           </div>
         </div>
       </animated.div>
