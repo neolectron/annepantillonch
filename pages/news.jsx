0dd1e21650de76224badac3cd84bc1d8e512816e
neolectron <neolectron@codinglab.io>
2021-02-05 10:44:08 +0100

    feat(news): Back button on news page

--- pages/news.jsx
+++ pages/news.jsx
@@ -1,6 +1,7 @@
 import { getPostListByTags } from '../lib/ghost.js';
 import Layout from '../components/Layout/Layout.jsx';
 import RichContent from '../components/RichContent/RichContent.jsx';
+import Button from '../components/Button/Button.jsx';
 import Link from 'next/link';
 
 export default function News({ news }) {
@@ -8,6 +9,7 @@ export default function News({ news }) {
   return (
     <Layout title={'news'}>
       <div className="p-8">
+        <Button asLink href="/works" > &lt; Works </Button>
         {news.map((n) => 
           <Link key={n.slug} href={`/posts/${n.slug}`}>
             <a className={`block p-8 border border-gray-400 w-max bg-white rounded-sm`}>
