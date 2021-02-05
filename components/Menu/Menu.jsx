386ddb5285ab61002e1111e8b674040a89d04ebf
neolectron <neolectron@codinglab.io>
2021-02-05 03:52:53 +0100

    feat(menu): add youtube link

--- components/Menu/Menu.jsx
+++ components/Menu/Menu.jsx
@@ -93,6 +93,13 @@ const Menu = ({children, Bgtransparent}) => {
                   v20.341H37.29v27.585h23.814v70.761H89.584z`} />
                 </svg>
               </a>
+              <a href="https://www.youtube.com/channel/UCJ7Zo_T1yTfsNVWu6REvi_w" target="_blank" className="m-2 transform hover:scale-110">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                  <path fill="currentColor" d={`M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356
+                  2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62
+                  4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z`} />
+                </svg>
+              </a>
           </div>
         </div>
       </animated.div>
