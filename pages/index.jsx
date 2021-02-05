552305bef193116c31109d3c0a286c21360acc2f
neolectron <neolectron@codinglab.io>
2021-02-05 02:01:18 +0100

    feat(index): customisable background and article

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import Layout from '../components/Layout/Layout.jsx';
 import { useCallback, useEffect } from 'react';
 import { animated, config, useTrail } from 'react-spring';
-import { getPageBySlug } from '../lib/ghost.js';
+import { getPageBySlug, getPostListByTags } from '../lib/ghost.js';
 import Flickity from 'react-flickity-component';
+import styles from '../styles/markdown.module.css';
 
-export default function Home({ page }) {
+export default function Home({ page, news }) {
 
   const [[first, second]] = useTrail(2, () => ({
     from: { opacity: 0, transform: -30 },
@@ -15,34 +16,34 @@ export default function Home({ page }) {
 
   const translate = useCallback(x => `translateX(${x}px)`);
 
-  console.log(page.imgs)
-
   return (
     <Layout menuBgTransparent>
-      <div className="block absolute h-screen w-screen top-0 left-0">
+
+      <div className="block fixed h-screen w-screen top-0 left-0">
         <Flickity static className="h-full w-full"
         options={{
           draggable: false,
           fade: true,
-          autoPlay: true,
+          autoPlay: 7000,
           setGallerySize: false,
           prevNextButtons: false,
           pageDots: false,
           imagesLoaded: true,
         }}>
           {page.imgs.map((img) => 
-            <div key={img.src} className="w-full h-full overflow-hidden">
-              <div style={{ backgroundImage: `url(${img.src})` }}
-                className={`w-full h-full bg-cover bg-pulse origin-right`}>
+              <div key={img.src} className="w-full h-full overflow-hidden">
+                <div style={{ backgroundImage: `url(${img.src})` }}
+                  className={`w-full h-full bg-cover bg-pulse origin-right`}>
+                </div>
               </div>
-            </div>
           )}
         </Flickity>
       </div>
-      <div className="relative h-screen flex overflow-hidden">
 
-        <div className="ml-8 mr-4 flex flex-col justify-center text-white">
-          <animated.h1 style={{ opacity: first.opacity, transform: first.transform.interpolate(translate) }}
+      <div className="relative flex">
+        <div className="ml-8 mr-4 flex flex-col justify-center items-center text-white">
+
+          <animated.h1 style={{ marginTop: '50%', opacity: first.opacity, transform: first.transform.interpolate(translate) }}
             className="opacity-0 text-5xl md:text-8xl font-bold px-4 border-b border-opacity-30 border-white">
             Anne Pantillon
           </animated.h1>
@@ -50,8 +51,18 @@ export default function Home({ page }) {
             className="opacity-0 text-2xl text-gray-300 shadow-xl px-4">
             Artiste Plasticienne 
           </animated.h2>
-        </div>
 
+          {news && <div 
+            className={`${styles.markdown} text-xl border-white border bg-white bg-opacity-30
+            p-8 my-14 overflow-hidden overflow-ellipsis
+            transition-opacity opacity-30 hover:opacity-100
+            hover:text-black hover:bg-opacity-70
+            cursor-pointer`}
+            style={{maxWidth: 600,}}
+            dangerouslySetInnerHTML={{__html: news.html}}>
+          </div>}
+
+        </div>
       </div>
     </Layout>
   )
@@ -61,8 +72,12 @@ export default function Home({ page }) {
 export async function getStaticProps() {
 
   const page = await getPageBySlug('accueil');
+  const [ news ] = await getPostListByTags('featured');
 
   return {
-    props: { page }
+    props: { 
+      page: page || {imgs: [{src: '/technique.png'}]}, 
+      news: news || null,
+    }
   };
 }
