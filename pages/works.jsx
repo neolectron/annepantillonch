14735f8f892d4633c91472bda1bd09fd3b809c5d
neolectron <neolectron@codinglab.io>
2021-02-06 19:43:56 +0100

    feat(works): sort tags with static order

--- pages/works.jsx
+++ pages/works.jsx
@@ -41,25 +41,57 @@ export async function getStaticProps() {
   // also exclude the 'news' tag
   // and pagination data in the "meta" property,
   // because tag.slug is undefined
-  const tags = rawTags
+  const pageTags = rawTags
     .filter( (tag) => (
       tag.slug && tag.slug !== 'news' && pages.map(p => p.slug).includes(tag.slug)
     ));
+
+  // sort tags according to this order
+  // if they are not in this list, push them after 
+  const sortOrder = [
+    'interieur',
+    'oscillography',
+    'small-papers-dessins',
+    'performances',
+    'rivers-rocks',
+    'papiers-colles',
+    'livres',
+    'carnets-de-voyage',
+    'fashion-design',
+  ];
+
+  const sortedTags = pageTags.sort((a, b) => {
+
+    const indexA = sortOrder.indexOf(a.slug);
+    const indexB = sortOrder.indexOf(b.slug);
+
+    // if they are both not in the sorted array
+    // do not sort them
+    if (indexA === -1 && indexB === -1)
+      return 0;
+
+    // if they are both in the sorted array
+    // sort them
+    if (indexA !== -1 && indexB !== -1)
+      return indexA - indexB;
+    
+    // if the second is not found, return -1
+    // if the second is found return its index
+    return indexB;
+
+  })
 
   // if the tag does not have a feature image,
   // we'll look for a feature image in posts (searching from last to first).
   // if it still does not have one, we'll search for any images in a tagged post
   // (from last to first still)
   const imgs = await Promise.all(
-    tags.map(async (tag) => tag.feature_image || getLastImageForTag(tag.slug) || '/technique.png')
+    sortedTags.map(async (tag) => tag.feature_image || getLastImageForTag(tag.slug) || '/technique.png')
   );
-  const tagsWithImages = tags.map((tag, index) => ({...tag, feature_image: imgs[index]}));
 
   return {
     props: {
-      tagList: [
-        ...tagsWithImages,
-      ]
+      tagList: sortedTags.map((tag, index) => ({ ...tag, feature_image: imgs[index] }))
     }
   }
 
