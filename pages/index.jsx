99d92cbb28d3c7a237457896f69f1ed4dcdd93a4
neolectron <neolectron@codinglab.io>
2021-02-07 13:35:55 +0100

    feat(index): add /works link to make navigation more natural

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,7 +42,7 @@ export default function Home({ page, news }) {
         </Flickity>
       </div>
 
-      <div className="z-10 px-2 md:px-10 flex-grow flex flex-col justify-center items-start text-white">
+      <div className="relative z-10 px-2 md:px-10 flex-grow flex flex-col justify-center items-start text-white">
         <a.h1 style={{ marginTop: '25%', opacity: first.opacity, transform: first.transform.interpolate(translate) }}
           className="opacity-0 text-5xl md:text-8xl font-bold px-4 border-b border-opacity-30 border-white">
           Anne Pantillon
@@ -53,7 +53,7 @@ export default function Home({ page, news }) {
         </a.h2>
 
         {news && <Link href={`/posts/${news.slug}`} >
-          <a className={`p-4 my-14 border border-white bg-white
+          <a className={`p-4 my-14 w-full md:w-4/12 border border-white bg-white
           bg-opacity-70 md:bg-opacity-30 hover:bg-opacity-70 
           text-black md:text-white hover:text-black
           transition-opacity md:opacity-30 hover:opacity-100`}
@@ -62,7 +62,21 @@ export default function Home({ page, news }) {
           </a>
         </Link>}
       </div>
-      
+
+      <Link href="/works">
+        <a.a style={{ opacity: second.opacity, transform: second.transform.interpolate(translate) }} 
+        className="hidden md:block z-10 absolute bottom-14 right-14 py-2 px-4 cursor-pointer">
+          <div className={`flex items-center 
+          text-2xl text-gray-300 border-b border-white border-opacity-30
+          transform transition-transform hover:scale-105 `}>
+            Explorez mes oeuvres
+            <svg className="mx-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+            </svg>
+          </div>
+        </a.a>
+      </Link>
+
     </Layout>
   )
 }
