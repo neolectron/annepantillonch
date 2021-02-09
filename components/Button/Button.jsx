085705817d9280b856a25b0356e41912858e6202
neolectron <neolectron@codinglab.io>
2021-02-09 03:56:29 +0100

    fix(Button): remove outline

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import Icon from '../Icon/Icon.jsx';
 
 const Button = ({icon, reversed, children, text}) => (
-  <button className={`flex uppercase text-2xl text-gray-300 transform transition-transform hover:scale-105`}>
+  <button className={`flex uppercase text-2xl text-gray-300 transform transition-transform hover:scale-105 focus:outline-none`}>
     {children || text} <Icon reversed={reversed} name={icon} />
   </button>
 );
