e4f5f877de161cd701f30042d5e8669efcb96f45
neolectron <neolectron@codinglab.io>
2021-02-09 05:11:53 +0100

    fix(Article): fix spread props, added hideFigure props

--- components/Article/Article.jsx
+++ components/Article/Article.jsx
@@ -1,7 +1,13 @@
 import styles from './article.module.css';
 
-const Article = ({html, ...rest}) => (
-  !html ? null : <article className={styles.article} dangerouslySetInnerHTML={{__html: html}} {...rest} />
+const Article = ({html, className, hideFigure, ...rest}) => (
+  !html 
+  ? 
+    null 
+  : 
+    <article dangerouslySetInnerHTML={{ __html: html }}
+    className={`${styles.article} ${hideFigure ? styles.hideFigure : ''} ${className}`}
+    {...rest} />
 );
 
 export default Article;
