04b75aee10e0af4662e7454bbeb299315e7095fe
neolectron <neolectron@codinglab.io>
2021-02-09 06:20:53 +0100

    fix(Cartel): fix cartel newline

--- components/Cartel/Cartel.jsx
+++ components/Cartel/Cartel.jsx
@@ -1,5 +1,5 @@
 const Cartel = ({ caption, className, ...rest }) => (
-  <div className={`uppercase text-2xl ${className}`} {...rest}>
+  <div className={`uppercase text-2xl whitespace-pre-line ${className}`} {...rest}>
     {caption?.replace(/,/g, '\n')}
   </div>
 );
