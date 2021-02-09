ae2d547c1896132b7272e983f3f1f1c8b75efc2b
neolectron <neolectron@codinglab.io>
2021-02-09 04:38:40 +0100

    fix(Article): fix nullish bug

--- components/Article/Article.jsx
+++ components/Article/Article.jsx
@@ -1,7 +1,7 @@
 import styles from './article.module.css';
 
 const Article = ({html, ...rest}) => (
-  html && <article className={styles.article} dangerouslySetInnerHTML={{__html: html}} {...rest} />
+  !html ? null : <article className={styles.article} dangerouslySetInnerHTML={{__html: html}} {...rest} />
 );
 
 export default Article;
