8102205f2fda03dcc846b7d5dde66aa8f1625986
neolectron <neolectron@codinglab.io>
2021-02-10 16:34:34 +0100

    fix(Button): Button has optionnal text + size is max-content

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -2,8 +2,8 @@ import Icon from '../Icon/Icon.jsx';
 
 const Wrap = ({ asAnchor, children, ...rest }) => asAnchor ? <a {...rest}>{children}</a> : <button {...rest}>{children}</button>
 
-const Button = ({ icon, reversed, swaped, children, text, asAnchor, ...rest }) => (
-  <Wrap asAnchor={asAnchor} className={`flex uppercase text-2xl 
+const Button = ({ icon, reversed, swaped, children, text = null, asAnchor, ...rest }) => (
+  <Wrap asAnchor={asAnchor} className={`flex w-max uppercase text-2xl 
   focus:outline-none transform transition-transform hover:scale-105 cursor-pointer`}
   {...rest}>
     {swaped && (children || text)}
