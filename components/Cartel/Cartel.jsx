28a6cf0741a01f4b426350c07256c47f3e400788
neolectron <neolectron@codinglab.io>
2021-02-12 01:59:45 +0100

    refactor(Cartel): clean code

--- components/Cartel/Cartel.jsx
+++ components/Cartel/Cartel.jsx
@@ -1,8 +1,14 @@
-const Cartel = ({ caption, className, ...rest }) => (
-  caption ? <div className={`p-4 text-xl whitespace-pre-line bg-white ${className}`} {...rest} 
-  >
-    {caption?.replace(/,/g, '\n')}
-  </div>
-  : null
-);
+const transformCaption = (caption) => caption?.replace(/,/g, '\n');
+
+const Cartel = ({ caption, children, className = '', ...rest }) => {
+  if(!children && !caption) return null;
+  return (
+    <div className={`p-4 text-xl whitespace-pre-line bg-white ${className}`}
+      {...rest}
+    >
+      {children || transformCaption(caption)}
+    </div>
+  );
+}
+
 export default Cartel;
