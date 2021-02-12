e7ed111eab2bf7e6c6c495512a0cc3a82d62f672
neolectron <neolectron@codinglab.io>
2021-02-12 02:00:15 +0100

    feat(Icon): tells you if you call invalid icon

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -6,6 +6,10 @@ const Icon = ({name, reversed, width = 25, height = 25}) => {
 
   const Image = icons[name];
 
+  if(!Image) {
+    throw new Error(`Icon ${name} does not exist, or is not imported in index.js`);
+  }
+
   return (
     <Image className={reversed ? styles.white : styles.black} width={width} height={height} />
   );
