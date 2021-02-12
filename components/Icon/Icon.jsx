2dcc2a5022ae134ee446b0522e204360a24feaaa
neolectron <neolectron@codinglab.io>
2021-02-12 16:00:26 +0100

    fix(Icon): fix icon color heritage from currentColor

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -2,7 +2,7 @@ import * as icons from './index';
 
 import styles from './icon.module.css';
 
-const Icon = ({name, reversed, width = 25, height = 25}) => {
+const Icon = ({name, reversed, width = 25, height = 25, className = ''}) => {
 
   const Image = icons[name];
 
@@ -11,7 +11,7 @@ const Icon = ({name, reversed, width = 25, height = 25}) => {
   }
 
   return (
-    <Image className={reversed ? styles.white : styles.black} width={width} height={height} />
+    <Image className={reversed ? styles.white : styles.black} width={width} height={height} className={className} />
   );
 };
 
