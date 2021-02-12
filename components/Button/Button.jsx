817ee2171a0838d58abcea9970647706a5717c9a
neolectron <neolectron@codinglab.io>
2021-02-12 01:55:49 +0100

    refactor(Button): clean code

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -2,15 +2,17 @@ import Icon from '../Icon/Icon.jsx';
 
 const Wrap = ({ asAnchor, children, ...rest }) => asAnchor ? <a {...rest}>{children}</a> : <button {...rest}>{children}</button>
 
-const Button = ({ icon, reversed, swaped, children, text = null, asAnchor, ...rest }) => (
-  <Wrap asAnchor={asAnchor} className={`flex w-max uppercase text-2xl 
-  focus:outline-none transform transition-transform hover:scale-105 cursor-pointer`}
-  {...rest}>
-    {swaped && (children || text)}
-    <Icon reversed={reversed} name={icon} />
-    {swaped || (children || text)}
-  </Wrap>
-);
+const Button = ({ icon, reversed, swaped, children = null, text = null, asAnchor, ...rest }) => {
+  return (
+    <Wrap asAnchor={asAnchor} className={`flex w-max uppercase text-2xl 
+    focus:outline-none transform transition-transform hover:scale-105 cursor-pointer`}
+    {...rest}>
+      {swaped || <Icon reversed={reversed} name={icon} />}
+      {text || children}
+      {swaped && <Icon reversed={reversed} name={icon} />}
+    </Wrap>
+  )
+}
 
 
 export default Button;
