b7af95c5fae8d82b7c97a3182e308ded16c7ebff
neolectron <neolectron@codinglab.io>
2021-02-18 13:12:10 +0100

    fix(index): top left title not clickable

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,64 +20,79 @@ export default function Home({ page, news }) {
   const translate = useCallback(x => `translateX(${x}px)`);
 
   return (
-    <Layout menuBgTransparent >
-
+    <Layout menuBgTransparent>
       <div className="z-0 pointer-events-none fixed h-screen w-screen top-0 left-0">
-        <Flickity static className="h-full w-full"
-        options={{
-          draggable: false,
-          fade: true,
-          autoPlay: 7000,
-          setGallerySize: false,
-          prevNextButtons: false,
-          pageDots: false,
-          imagesLoaded: true,
-        }}>
-          {page.imgs.map((img) => 
+        <Flickity
+          static
+          className="h-full w-full"
+          options={{
+            draggable: false,
+            fade: true,
+            autoPlay: 7000,
+            setGallerySize: false,
+            prevNextButtons: false,
+            pageDots: false,
+            imagesLoaded: true,
+          }}
+        >
+          {page.imgs.map((img) => (
             <div key={img.src} className="w-full h-full overflow-hidden">
-              <div style={{ backgroundImage: `url(${img.src})` }}
-                className={`w-full h-full bg-cover bg-pulse origin-right`}>
-              </div>
+              <div
+                style={{ backgroundImage: `url(${img.src})` }}
+                className={`w-full h-full bg-cover bg-pulse origin-right`}
+              ></div>
             </div>
-          )}
+          ))}
         </Flickity>
       </div>
 
       <div className="relative z-10 flex-grow grid grid-cols-1 md:grid-cols-4 content-center">
-        <Link href="/works">
-          <a.a style={{ opacity: second.opacity, transform: second.transform.interpolate(translate) }}
-            className="hidden md:block z-10 fixed top-1 left-10 py-2 cursor-pointer text-gray-300">
-            <Button reversed swaped text="ANNE PANTILLON" />
-          </a.a>
-        </Link>
+        <a.div
+          style={{
+            opacity: second.opacity,
+            transform: second.transform.interpolate(translate),
+          }}
+          className="text-2xl hidden md:block z-10 fixed top-1 left-10 py-2 text-gray-300"
+        >
+          ANNE PANTILLON
+        </a.div>
 
         <Link href="/works">
-          <a.a style={{ opacity: second.opacity, transform: second.transform.interpolate(translate) }}
-            className="hidden md:block z-10 fixed top-1 right-14 py-2 px-4 cursor-pointer text-gray-300">
+          <a.a
+            style={{
+              opacity: second.opacity,
+              transform: second.transform.interpolate(translate),
+            }}
+            className="hidden md:block z-10 fixed top-1 right-14 py-2 px-4 text-gray-300"
+          >
             <Button reversed swaped icon="right" text="Travaux" />
           </a.a>
         </Link>
 
         <div className="px-2 col-span md:px-10 col-span-2 flex flex-col justify-center col-start-2">
-          <a.h1 style={{ opacity: first.opacity, transform: first.transform.interpolate(translate) }}
-            className="text-center text-5xl md:text-8xl text-white">
+          <a.h1
+            style={{
+              opacity: first.opacity,
+              transform: first.transform.interpolate(translate),
+            }}
+            className="text-center text-5xl md:text-8xl text-white"
+          >
             Actualités
           </a.h1>
 
-          {news && 
+          {news && (
             <div className="grid grid-cols-1 md:grid-cols-4">
-              <Link href={`/posts/${news.slug}`} >
+              <Link href={`/posts/${news.slug}`}>
                 <a className="col-start-2 col-span-2 p-4 pt-3 block shadow-xl bg-black bg-opacity-30 text-white">
                   <Article html={news.html} />
                 </a>
               </Link>
             </div>
-          }
+          )}
         </div>
       </div>
-
     </Layout>
-  )
+  );
 }
 
 // Fetch necessary data for the current page
