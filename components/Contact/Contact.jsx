771339981d3a9a8fbd65da1e3d7e38adb59c4ea8
neolectron <xeolectron@gmail.com>
2021-03-29 15:40:02 +0200

    feat(Contact): no more transparent background

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -2,28 +2,32 @@ import Button from '../Button/Button.jsx';
 
 const Contact = () => {
   return (
-    <div className={`flex flex-col mt-12 text-white
-    bg-black bg-opacity-30 overflow-hidden
+    <div
+      className={`flex flex-col mt-12 text-black
+    bg-white overflow-hidden
       md:h-full md:mr-12 md:mt-0 md:w-auto`}
-    style={{ minWidth: '400px' }}>
-
+      style={{ minWidth: '400px' }}
+    >
       <img src="profil.jpg" alt="photo de profil" className="h-48" />
       <div className="h-full flex flex-col items-center justify-center p-4 pt-8">
-
         <address className="text-center text-xl not-italic">
-          Anne Pantillon<br />
-          Rue de Sébeillon 9a<br />
+          Anne Pantillon
+          <br />
+          Rue de Sébeillon 9a
+          <br />
           1004 Lausanne, Suisse
         </address>
 
-        <Button asAnchor href="mailto:atelier.annepantillon@gmail.com"
-        className="normal-case transform-none underline text-xl">
+        <Button
+          asAnchor
+          href="mailto:atelier.annepantillon@gmail.com"
+          className="normal-case transform-none underline text-xl"
+        >
           atelier.anne.pantillon@gmail.com
         </Button>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
