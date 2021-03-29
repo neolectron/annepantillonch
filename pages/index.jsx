ba0fbe7351069b28541cbbd769e2d37c201bb2de
neolectron <xeolectron@gmail.com>
2021-03-29 17:21:19 +0200

    refactor(indexPage): now uses Header, and remove menu transparent BG

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,12 @@
 import Layout from '../components/Layout/Layout.jsx';
 import Article from '../components/Article/Article.jsx';
+import Header from '../components/Header/Header.jsx';
 import { useCallback } from 'react';
 import { a, config, useTrail } from 'react-spring';
 import { getPageBySlug, getPostListFeatured } from '../lib/ghost.js';
-import Flickity from 'react-flickity-component';
 import Link from 'next/link';
-import Button from '../components/Button/Button.jsx';
-
 
 export default function Home({ page, news }) {
-
   const [[first, second]] = useTrail(2, () => ({
     from: { opacity: 0, transform: -30 },
     to: { opacity: 1, transform: 0 },
@@ -17,58 +14,12 @@ export default function Home({ page, news }) {
     config: config.molasses,
   }));
 
-  const translate = useCallback(x => `translateX(${x}px)`);
+  const translate = useCallback((x) => `translateX(${x}px)`);
 
   return (
-    <Layout menuBgTransparent>
-      <div className="z-0 pointer-events-none fixed h-screen w-screen top-0 left-0">
-        <Flickity
-          static
-          className="h-full w-full"
-          options={{
-            draggable: false,
-            fade: true,
-            autoPlay: 7000,
-            setGallerySize: false,
-            prevNextButtons: false,
-            pageDots: false,
-            imagesLoaded: true,
-          }}
-        >
-          {page.imgs.map((img) => (
-            <div key={img.src} className="w-full h-full overflow-hidden">
-              <div
-                style={{ backgroundImage: `url(${img.src})` }}
-                className={`w-full h-full bg-cover bg-pulse origin-right`}
-              ></div>
-            </div>
-          ))}
-        </Flickity>
-      </div>
-
-      <div className="relative z-10 flex-grow grid grid-cols-1 md:grid-cols-4 content-center">
-        <a.div
-          style={{
-            opacity: second.opacity,
-            transform: second.transform.interpolate(translate),
-          }}
-          className="text-2xl hidden md:block z-10 fixed top-1 left-7 py-2 text-gray-300"
-        >
-          ANNE PANTILLON
-        </a.div>
-
-        <Link href="/works">
-          <a.a
-            style={{
-              opacity: second.opacity,
-              transform: second.transform.interpolate(translate),
-            }}
-            className="hidden md:block z-10 fixed top-1 right-14 py-2 px-4 text-gray-300"
-          >
-            <Button reversed swaped icon="right" text="Travaux" />
-          </a.a>
-        </Link>
-
+    <Layout className="bg-anne bg-cover text-white">
+      <Header backText="ANNE PANTILLON" goText="TRAVAUX" goTo="/works" />
+      <div className="flex-grow grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 content-center bg-cover bg-no-repeat">
         <div className="px-2 col-span md:px-10 col-span-2 flex flex-col justify-center col-start-2">
           <a.h1
             style={{
@@ -97,14 +48,13 @@ export default function Home({ page, news }) {
 
 // Fetch necessary data for the current page
 export async function getStaticProps() {
-
   const page = await getPageBySlug('accueil');
   const [news] = await getPostListFeatured();
 
   return {
-    props: { 
-      page: page || {imgs: [{src: '/technique.png'}]}, 
+    props: {
+      page: page || { imgs: [{ src: '/technique.png' }] },
       news: news || null,
-    }
+    },
   };
 }
