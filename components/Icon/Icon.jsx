d04746b4468322d369e0511805fe88c739766bbc
neolectron <xeolectron@gmail.com>
2021-05-17 11:02:23 +0200

    fix(Icon): no duplicate props on component

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -2,19 +2,16 @@ import * as icons from './index';
 
 import styles from './icon.module.css';
 
-const Icon = ({name, reversed, width = 25, height = 25, className = ''}) => {
-
-  if(!name) return null;
+const Icon = ({ name, reversed, width = 25, height = 25, className = '' }) => {
+  if (!name) return null;
 
   const Image = icons[name];
 
-  if(!Image) {
+  if (!Image) {
     throw new Error(`Icon ${name} does not exist, or is not imported in /components/Icon/index.js`);
   }
 
-  return (
-    <Image className={reversed ? styles.white : styles.black} width={width} height={height} className={className} />
-  );
+  return <Image className={className || (reversed ? styles.white : styles.black)} width={width} height={height} />;
 };
 
 export default Icon;
