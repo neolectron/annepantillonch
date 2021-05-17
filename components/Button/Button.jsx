b0996db2e5a4a89088db62b57f8238c9bb7459f0
neolectron <xeolectron@gmail.com>
2021-05-17 11:01:18 +0200

    feat(Button): pass iconClassName prop to icon className

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,18 +1,31 @@
 import Icon from '../Icon/Icon.jsx';
 
-const Wrap = ({ asAnchor, children, ...rest }) => asAnchor ? <a {...rest}>{children}</a> : <button {...rest}>{children}</button>
+const Wrap = ({ asAnchor, children, ...rest }) =>
+  asAnchor ? <a {...rest}>{children}</a> : <button {...rest}>{children}</button>;
 
-const Button = ({ icon, reversed, swaped, children = null, text = null, asAnchor, className = '', ...rest }) => {
+const Button = ({
+  icon,
+  reversed,
+  swaped,
+  children = null,
+  text = null,
+  asAnchor,
+  className = '',
+  iconClassName = '',
+  ...rest
+}) => {
   return (
-    <Wrap asAnchor={asAnchor} className={`flex w-max uppercase text-2xl 
+    <Wrap
+      asAnchor={asAnchor}
+      className={`flex w-max uppercase text-2xl 
     focus:outline-none transform transition-transform hover:scale-105 cursor-pointer ${className}`}
-    {...rest}>
-      {swaped || <Icon reversed={reversed} name={icon} />}
+      {...rest}
+    >
+      {swaped || <Icon reversed={reversed} name={icon} className={iconClassName} />}
       {text || children}
-      {swaped && <Icon reversed={reversed} name={icon} />}
+      {swaped && <Icon reversed={reversed} name={icon} className={iconClassName} />}
     </Wrap>
-  )
-}
-
+  );
+};
 
 export default Button;
