b57f0e35fe4fefff747fc9937e527f389d3ebd0f
neolectron <xeolectron@gmail.com>
2021-05-22 09:05:54 +0200

    fix(Cartel): fix bad rendering of html &num elements

--- components/Cartel/Cartel.jsx
+++ components/Cartel/Cartel.jsx
@@ -3,8 +3,8 @@ const transformCaption = (caption) => caption?.replace(/,/g, '\n').replace('vend
 const Cartel = ({ caption, children, className = '', ...rest }) => {
   if (!children && !caption) return null;
   return (
-    <div className={className || 'p-4 text-xl whitespace-pre-line bg-white'} {...rest}>
-      {children || transformCaption(caption)}
+    <div className={className || 'p-4 text-xl whitespace-pre-line flex bg-white'} {...rest}>
+      {children || <div dangerouslySetInnerHTML={{ __html: transformCaption(caption) }}></div>}
       {caption?.includes('vendu') && <div className="absolute w-3 h-3 bg-red-500 rounded-full bottom-2 right-2"></div>}
     </div>
   );
