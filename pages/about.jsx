dd589c60558b1b25ef552e8c60eb4184454ebb47
neolectron <neolectron@codinglab.io>
2022-02-15 16:45:25 +0100

    feat(AboutPage): add Accordion

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,13 +3,22 @@ import Layout from '../components/Layout/Layout.jsx';
 import Article from '../components/Article/Article.jsx';
 import Header from '../components/Header/Header.jsx';
 import Footer from '../components/Footer/Footer.jsx';
+import Accordion from '../components/Accordion/Accordion.jsx';
 
-const AboutPage = ({ about }) => (
+const AboutPage = ({ cv, parcours }) => (
   <Layout title="a propos">
     <Header backTo="/" backText="Accueil" />
 
-    <div className="grid grid-cols-1 px-2 md:px-10 md:grid-cols-6">
-      <Article html={about.html} className="md:col-start-3 md:col-span-3" />
+    <div className="md:px-10 flex justify-center px-2">
+      <div className="w-full max-w-3xl">
+        <Accordion names={['Mon Parcours', 'CV']}>
+          <Article
+            html={parcours.html}
+            className="md:col-start-3 md:col-span-3"
+          />
+          <Article html={cv.html} className="md:col-start-3 md:col-span-3" />
+        </Accordion>
+      </div>
     </div>
 
     <Footer backTop />
@@ -19,6 +28,9 @@ const AboutPage = ({ about }) => (
 export default AboutPage;
 // Fetch necessary data for the blog post using tags
 export async function getStaticProps() {
-  const about = await getPageBySlug('a-propos');
-  return { props: { about } };
+  // const about = await getPageBySlug('a-propos');
+  const cv = await getPageBySlug('cv');
+  const parcours = await getPageBySlug('parcours');
+
+  return { props: { cv, parcours } };
 }
